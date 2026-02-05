42f905be2b1126f9674f6ffac9f1f7d987dfd622
Dabhi Amit <amitdabhi526@gmail.com>
2026-02-05 14:38:57 +0530

    express intro done

---
+++ 03_Express-intro/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("hello from express server");
+});
+
+app.get("/about", (req, res) => {
+  res.json("about page");
+});
+
+const port = 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("server running on port", port);
+});
